1e55e456f6fbd0542f3ec4e296074063ca6ca669
alsk-31 <140357564+alsk-31@users.noreply.github.com>
2023-12-24 00:39:05 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,85 @@
 document.addEventListener('DOMContentLoaded',function(){
-  
-    //포트폴리오 롤링컨텐츠(6) 스크립트 : swiper
-   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 24,
-    autoplay: {
-    delay: 5000,
-    },   
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  }
+
+// 1. 네비게이션 메뉴들을 querySelectorAll을 통해 변수에 담는다.
+const gnbItems = document.querySelectorAll(".gnb__item");
+// 2. 섹션들을 전부 querySelectorAll을 통해 변수에 담는다.
+const sections = document.querySelectorAll("section");
+
+// 3. forEach 문을 통해 한번씩 순회한다.
+// 이때 index도 같이 가져온다.
+gnbItems.forEach((gnbItem, index) => {
+
+  //4. 네비게이션 메뉴에 클릭 이벤트를 준다.
+  gnbItem.addEventListener("click", (e) => {
+    // 5. 메뉴를 a 태그에 만들었기 때문에, 
+    // 태그의 기본 동작(링크 연결) 방지를 위해 preventDefault를 추가한다.
+    e.preventDefault();
+    
+    // 6. 섹션들 중 네비게이션 메뉴의 index 에 해당하는 섹션의 높이값을 구하고,
+    // 네비게이션 높이만큼 값을 빼준다.
+    const sectionTop = sections[index].offsetTop - 70;
+    
+    // 7. 해당 위치로 스크롤을 이동시킨다.
+    window.scroll({ top: sectionTop });
   });
+});
 
-
+ // Modal을 가져옵니다.
+ var modals = document.getElementsByClassName("modal");
+ // Modal을 띄우는 클래스 이름을 가져옵니다.
+ var btns = document.getElementsByClassName("btn");
+ // Modal을 닫는 close 클래스를 가져옵니다.
+ var spanes = document.getElementsByClassName("close");
+ var funcs = [];
+  
+ // Modal을 띄우고 닫는 클릭 이벤트를 정의한 함수
+ function Modal(num) {
+   return function() {
+     // 해당 클래스의 내용을 클릭하면 Modal을 띄웁니다.
+     btns[num].onclick =  function() {
+         modals[num].style.display = "block";
+         console.log(num);
+     };
+  
+     // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
+     spanes[num].onclick = function() {
+         modals[num].style.display = "none";
+     };
+   };
+ }
+  
+ // 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
+ for(var i = 0; i < btns.length; i++) {
+   funcs[i] = Modal(i);
+ }
+  
+ // 원하는 Modal 수만큼 funcs 함수를 호출합니다.
+ for(var j = 0; j < btns.length; j++) {
+   funcs[j]();
+ }
+  
+ // Modal 영역 밖을 클릭하면 Modal을 닫습니다.
+ window.onclick = function(event) {
+   if (event.target.className == "modal") {
+       event.target.style.display = "none";
+   }
+ };
+ 
+ 
+   //모달창 스와이퍼
+   var swiper = new Swiper(".mySwiper", {
+     pagination: {
+       el: ".swiper-pagination",
+       type: "fraction"
+     },
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev"
+     }
+   });
+   
+   
+ 
 
 //스크롤이벤트
   let btn = document.getElementById('up-too');
@@ -67,33 +133,6 @@ document.addEventListener('DOMContentLoaded',function(){
       }
      },20)
   }
-
-  let el = $("section");
-  $(el).each(function (index) {
-    $(this).on("mousewheel", function () {
-      console.log(event.wheelDelta);
-      let moveTop = $(window).scrollTop();
-      let eleselector = $(el).eq(index);
-      if (event.wheelDelta < 0) {
-        if ($(eleselector).next() != undefined) {
-          try {
-            moveTop = $(eleselector).next().offset().top;
-          } catch (e) {}
-        }
-      } else {
-        if ($(eleselector).prev() != undefined) {
-          try {
-            moveTop = $(eleselector).prev().offset().top;
-          } catch (e) {}
-        }
-      }
-      $("html,body")
-        .stop()
-        .animate({ scrollTop: moveTop + "px" }, 800);
-    });
-  });
-
-
 
 })
 
