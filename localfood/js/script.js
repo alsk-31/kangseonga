c1aab8920624d09b547c89a08060c4c7851a573c
alsk-31 <140357564+alsk-31@users.noreply.github.com>
2024-08-11 21:12:14 +0900

    first commit

---
+++ localfood/js/script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    let navList = document.querySelector(".nav > ul");
+
+    navList.addEventListener("mouseover",()=>{
+        navList.querySelectorAll(".submenu").forEach(sub=>{
+            sub.style.height="155px";
+        });
+        document.getElementById("header").classList.add("on")
+    });
+    navList.addEventListener("mouseout", () => {
+        navList.querySelectorAll(".submenu").forEach(sub => {
+            sub.style.height = "0px";
+        });
+        document.getElementById("header").classList.remove("on");
+    });
+}
